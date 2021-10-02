c5726a1533a0b7fe869ff8b0948e8695b6d6f789
AlexM <tempthing0@gmail.com>
2021-10-02 15:50:35 -0700

    Tweaked movement and shooting

--- src/Code/World/World.gd
+++ src/Code/World/World.gd
@@ -4,9 +4,18 @@ extends Node2D
 func _ready():
 	$GUI.set_player($Player)
 	$Player.connect("shot", self, "_on_player_shot")
-
+	$Player.connect("landed", self, "_on_player_landed")
+	$Player.connect("jumped", self, "_on_player_jumped")
 
 func _on_player_shot():
-	$Camera2D.add_trauma(0.4)
+	$Camera2D.add_trauma(0.2)
+	$Camera2D.add_zoom(0.6)
+
+func _on_player_jumped():
+	$Camera2D.add_trauma(0.1)
+
+func _on_player_landed():
+	$Camera2D.add_trauma(0.1)
+
 
 

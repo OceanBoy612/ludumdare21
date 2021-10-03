2ba7299d4d40c86b0acae853ceb6f8cdcc951259
Yonatan Attali <yonatan.attali@gmail.com>
2021-10-02 22:24:11 -0400

    bullets don't break tiles anymore

--- src/Code/Player/Bullet/Bullet.gd
+++ src/Code/Player/Bullet/Bullet.gd
@@ -12,8 +12,8 @@ func _physics_process(delta):
 	$Sprite.rotate(delta * deg2rad(rotation_speed))
 	var col: KinematicCollision2D = move_and_collide(Vector2(speed,0).rotated(rotation))
 	if col:
-		if col.collider.has_method("break_tile"):
-			col.collider.break_tile(col.position - (col.normal * 4))
+#		if col.collider.has_method("break_tile"): # to break the tiles
+#			col.collider.break_tile(col.position - (col.normal * 4))
 		if col.collider.has_method("hurt"):
 			col.collider.hurt()
 		die()
